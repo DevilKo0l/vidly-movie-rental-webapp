edea3c35259fd2fb1262fd0923f5691f2894522d
Nguyen Cao <nguyen.tcao96@gmail.com>
2023-09-04 00:08:39 +0200

    Initial commit for Entity Framework

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        public ApplicationDbContext() : base("VidlyMovieRentalDb") { }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
