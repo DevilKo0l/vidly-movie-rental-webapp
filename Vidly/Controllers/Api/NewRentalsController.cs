41dfad20ad27b7b0cc937fc1bfd9f9426ddbdc89
DevilKo0l <nguyen.tcao96@gmail.com>
2024-01-10 22:33:12 +0100

    Update NewRentalsController API

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -16,12 +16,19 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);                     
+                    
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id)).ToList();                      
 
             foreach(var movie in movies)
             {
+                if(movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
@@ -34,6 +41,7 @@ namespace Vidly.Controllers.Api
 
             _context.SaveChanges();
             return Ok();
+
         }
     }
 }
